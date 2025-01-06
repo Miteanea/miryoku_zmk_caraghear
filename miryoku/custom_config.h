cdaa2d9f2e6f366de226582cfa2d8ced677ab1c3
Dmitri Caragheaur <39595454+Miteanea@users.noreply.github.com>
2025-01-06 13:11:03 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -25,7 +25,7 @@ U_NA,              U_NA,              U_NA,              &kp Y,             &kp 
 U_NP,              U_NP,              U_NA,              &kp LCTRL,         &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
 
 // Direct custom layer access
-#define MIRYOKU_LAYOUTMAPPING_CORNE(\
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
 K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09, \
 K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19, \
 K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29, \
